52f8ae7ed8d3563eb3034fed2ee87d622a9a46b6
BishoM <bishomoise84@gmail.com>
2023-04-04 09:03:10 +0200

    implicity intent for share to

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,9 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnShowToast = findViewById<Button>(R.id.btnShowToast)
+        val sendToNext = findViewById<Button>(R.id.sendToNext)
+        val userMessage = findViewById<EditText>(R.id.userMessage)
+        val btnShare = findViewById<Button>(R.id.btnShare)
         btnShowToast.setOnClickListener {
             Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
         }
+
+        sendToNext.setOnClickListener{
+            val message: String = userMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+
+        btnShare.setOnClickListener{
+            val message: String = userMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to: "))
+         }
     }
 
 }
--- app/src/main/java/com/example/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/example/msgshareapp/SecondActivity.kt
@@ -1,4 +1,21 @@
 package com.example.msgshareapp
 
-class SecondActivity {
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val txtUserMessage = findViewById<TextView>(R.id.txtUserMessage)
+        val bundle: Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txtUserMessage.text = msg
+    }
 }
